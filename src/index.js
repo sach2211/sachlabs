07e148f191c735c8e8de2c914f772c0f1badcf6a
sach11 <sachin.chopra@myntra.com>
2018-04-03 18:52:32 +0530

    Webpack setup to transpile React

--- src/index.js
+++ src/index.js
@@ -1,19 +1,28 @@
+/**
+ * Import express and initialize it.
+ */
 const express = require("express");
 const app = express();
 
+
+// Path module to construct absolute paths from relative ones.
 const path = require("path");
 
 
+// Make this directory available.
+// Thus, index.html, chunk js and other files in this directory will be directly available.
+// This will also allow our html file to request the js file.
+
 app.use(express.static(path.join(__dirname, '../public')));
 
+// request to root are redirect to index.html
 app.get("/", (req, res, next) => {
     // res.sendFile (path.join(__dirname, '../public/index.html'));
     res.redirect('/index.html');
 });
 
 app.get("/projects", (req, res, next) => {
-    res.sendFile(__dirname + '/views/index.html');
-    // res.send("This site will be up in a few days !! Thanks for your patience")
+    res.status(200).send('Under maintenance, will be back in a few days');
 });
 
 // 404 for all other routes.
@@ -21,6 +30,7 @@ app.get("*",  (req, res, next) => {
     res.status(404).send("Wrong URL maybe ?");
 })
 
+// Start on the port specified by the environment.
 const PORT = process.env.port || 3010;
 app.listen(PORT, () => {
     console.log("Server is up and listening on port ", PORT);
--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,10 @@
 const path = require('path');
 
 module.exports = {
-  
+  // The entry path - should be a absolute path.
   entry: path.join(__dirname + '/src/views/index.js'),
   
+  // The output path object.
   output: {
     path: path.join(__dirname + '/public/'),
     filename: 'chunk.js'
@@ -13,6 +14,8 @@ module.exports = {
     rules: [
 
       {
+        // Use babel to transpile JSX and ES6 features.
+        // The presets are important - specified in .babelrc
         test: /\.(js|jsx)$/,
         exclude: path.join(__dirname + '/node_modules/'),
         use: 'babel-loader'
