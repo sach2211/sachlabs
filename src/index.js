f566672efacc9d41454f253edce15361c48f11c0
sach11 <sachin.chopra@myntra.com>
2018-04-03 16:34:12 +0530

    express routing.

--- src/index.js
+++ src/index.js
@@ -1,9 +1,23 @@
 const express = require("express");
 const app = express();
 
+const path = require("path");
+// const html = require("./views/index.html");
+
 app.get("/", (req, res, next) => {
-    res.send("This site will be up in a few days !! Thanks for your patience")
+    res.sendFile(__dirname + '/views/index.html');
+    // res.send("This site will be up in a few days !! Thanks for your patience")
 });
+
+app.get("/projects", (req, res, next) => {
+    res.sendFile(__dirname + '/views/index.html');
+    // res.send("This site will be up in a few days !! Thanks for your patience")
+});
+
+// 404 for all other routes.
+app.get("*",  (req, res, next) => {
+    res.status(404).send("Wrong URL maybe ?");
+})
 
 const PORT = process.env.port || 3010;
 app.listen(PORT, () => {
